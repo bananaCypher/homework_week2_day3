4719b78c5ca3c1d355240179612c36f0a17c4199
Callum Hogg <bananaCypher@gmail.com>
2015-11-25 15:20:11 +0000

    Added win conditions and re-arringed some methods to be private.

--- game.rb
+++ game.rb
@@ -1,69 +1,85 @@
 class Game
-   def initialize
-    @board = [ [nil,nil,nil], [nil,nil,nil], [nil,nil,nil] ]
-    @pieces = [:o, :x]
-    @turn = 0
-   end 
+    def initialize
+        @board = [ [nil,nil,nil], [nil,nil,nil], [nil,nil,nil] ]
+        @pieces = [:o, :x]
+        @turn = 0
+    end 
 
-   def display_board
-       row_strings = @board.map do |row|
-           row_to_string(row)
-       end
-       row_strings.join("\n_ _ _\n")
-   end
+    def display_board
+        row_strings = @board.map { |row| row_to_string(row) }
+        row_strings.join("\n_ _ _\n")
+    end
 
-   def check_board
-       nil_count = 0
-       #each column
-       @board.each do |column|
-            return false if column.select{|space| space==nil}.count > 0
-       end
-       puts "The board is full and a draw has been declared, the game has been reset."
-       reset
-   end
+    def place_piece(row, column)
+        if row > 2 || column > 2
+            puts "Can't place a piece outside of the board"
+            return
+        end
+        if @board[row][column]
+            puts "Already a piece there"
+            return
+        end 
+        symbol = @pieces[@turn % 2]
+        @board[row][column] = symbol 
+        puts display_board
+        puts "\n"
+        @turn += 1
+        if has_won?(symbol)
+            puts "#{symbol} has won the game! resetting the board."
+            reset
+            return
+        end
+        if board_full?
+            puts "The board is full and a draw has been declared, resetting the board."
+            reset
+            return
+        end
+    end
 
-   def place_piece(row, column)
-       if row > 2 || column > 2
-           puts "Can't place a piece outside of the board"
-           return
-       end
-       if @board[row][column]
-           puts "Already a piece there"
-           return
-       end 
-       @board[row][column] = @pieces[@turn % 2] 
-       @turn += 1
-       check_board
-   end
+    def reset
+        @board = [ [nil,nil,nil], [nil,nil,nil], [nil,nil,nil] ]
+        @turn = 0
+        @pieces.rotate!
+        puts "\n"
+        puts "New game:-"
+        puts display_board
+    end 
 
-   def reset
-       @board = [ [nil,nil,nil], [nil,nil,nil], [nil,nil,nil] ]
-       @turn = 0
-       @pieces.rotate!
-   end
 
-   def has_won?(symbol)
-       return true if horizontal_line?(symbol, @board) || vertical_line?(symbol)
-       return false
-   end
+    private
+    def row_to_string(row)
+        row_symbols = row.map do |piece|
+            piece || ' '
+        end
+        row_symbols.join('|')
+    end
 
-   def horizontal_line?(symbol, board)
-       board.any? {|row| row_has_winning_line(row, symbol)}
-   end
+    def has_won?(symbol)
+        return true if horizontal_line?(symbol, @board) || vertical_line?(symbol) || diagonal_line?(symbol)
+        return false
+    end
 
-   def row_has_winning_line(row, symbol)
-       row.all? {|square| square == symbol}
-   end
+    def horizontal_line?(symbol, board)
+        board.any? {|row| row_has_winning_line(row, symbol)}
+    end
 
-   def vertical_line?(symbol)
-       horizontal_line?(symbol, @board.transpose)
-   end
+    def row_has_winning_line(row, symbol)
+        row.all? {|square| square == symbol}
+    end
 
-   private
-       def row_to_string(row)
-          row_symbols = row.map do |piece|
-             piece || ' '
-          end
-          row_symbols.join('|')
-       end
+    def vertical_line?(symbol)
+        horizontal_line?(symbol, @board.transpose)
+    end
+
+    def diagonal_line?(symbol)
+        middle_piece = @board[1][1]
+        return false if middle_piece != symbol
+        top_left_and_bottom_right = (@board[0][0] == symbol) && (@board[2][2] == symbol)
+        top_right_and_bottom_left = (@board[0][2] == symbol) && (@board[2][0] == symbol)
+        top_left_and_bottom_right || top_right_and_bottom_left
+    end
+
+    def board_full?
+        @turn >= 9
+    end
 end
--- game_runner.rb
+++ game_runner.rb
@@ -7,7 +7,7 @@ game = Game.new()
 game.place_piece(0,0)
 
 # Display board
-puts game.display_board
+#puts game.display_board
 
 # Reset board
 puts game.reset
@@ -17,7 +17,7 @@ game.place_piece(3,3)
 
 # Rotate the starting piece when resetting
 game.place_piece(0,0)
-puts game.display_board
+#puts game.display_board
 
 # Automatically reset when the baord is full and tell us it's a draw
 game.place_piece(0,0)
@@ -31,25 +31,28 @@ game.place_piece(1,2)
 game.place_piece(2,2)
 
 piece = :o
-puts "checking if game is won #{game.has_won?(piece)}, expect to be false"
 game.place_piece(0,0)
 game.place_piece(1,0)
 game.place_piece(0,1)
 game.place_piece(1,1)
 game.place_piece(0,2)
-puts game.display_board
-puts "checking if game is won #{game.has_won?(piece)}, expect to be true"
-game.reset
+#puts game.display_board
 
 piece = :x
-puts "checking if game is won #{game.has_won?(piece)}, expect to be false"
 game.place_piece(0,0)
 game.place_piece(1,1)
 game.place_piece(1,0)
 game.place_piece(1,2)
 game.place_piece(2,0)
-puts game.display_board
-puts "checking if game is won #{game.has_won?(piece)}, expect to be true"
+#puts game.display_board
+
+piece = :o
+game.place_piece(0,0)
+game.place_piece(0,1)
+game.place_piece(1,1)
+game.place_piece(0,2)
+game.place_piece(2,2)
+#puts game.display_board
 
 
 binding.pry; ''
